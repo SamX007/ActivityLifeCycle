7d2435aa6d255ce2ea4345851f0c1918ebc43def
shamip <imsamip007@gmail.com>
2021-03-19 13:08:07 +0545

    Coding Challenge:

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
-## Two Activities Data Sent and Received
-![](app/src/main/res/drawable/task.gif)
+## Coding Challenge
+![](app/src/main/res/drawable/coding.gif)
--- app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
+++ app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
@@ -1,51 +1,105 @@
 package com.example.twoactivitieslifecycle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG =
-            MainActivity.class.getSimpleName();
-    private EditText mMessageEditText;
-    public static final String EXTRA_MESSAGE =
-            "com.example.android.twoactivities.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    public HashMap<String, Integer> map;
+    private TextView item;
+    private int list_order = 0;
+    public static final int confirmation = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        map = new HashMap<String, Integer>();
     }
 
-    public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivityForResult(intent, TEXT_REQUEST);
+    public void showitems(View view) {
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivityForResult(intent,confirmation);
     }
+
     @Override
     public void onActivityResult(int requestCode,
                                  int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(confirmation == requestCode)
             if (resultCode == RESULT_OK) {
-                String reply =
-                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
+                String item_name = data.getStringExtra("item_id");
+                if(!map.containsKey(item_name)){
+                    list_order++;
+                    map.put(item_name,list_order);
+                }
+                showitemlist();
+            }
+    }
+
+    public void showitemlist(){
+        for(HashMap.Entry<String,Integer> list : map.entrySet()){
+            String item_name = list.getKey();
+            int show_order = list.getValue();
+            switch (show_order){
+                case 1:
+                    item = findViewById(R.id.item1);
+                    break;
+                case 2:
+                    item = findViewById(R.id.item2);
+                    break;
+                case 3:
+                    item = findViewById(R.id.item3);
+                    break;
+                case 4:
+                    item = findViewById(R.id.item4);
+                    break;
+                case 5:
+                    item = findViewById(R.id.item5);
+                    break;
+                case 6:
+                    item = findViewById(R.id.item6);
+                    break;
+                case 7:
+                    item = findViewById(R.id.item7);
+                    break;
+                case 8:
+                    item = findViewById(R.id.item8);
+                    break;
+                case 9:
+                    item = findViewById(R.id.item9);
+                    break;
+                case 10:
+                    item = findViewById(R.id.item10);
+                    break;
+                default:
+                    break;
+            }
+            item.setText(show_order+". "+item_name);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("item_list",map);
+        outState.putInt("item_showorder",list_order);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if(savedInstanceState != null){
+            if(map.isEmpty()){
+                list_order = savedInstanceState.getInt("item_showorder");
+                map =(HashMap<String, Integer>) savedInstanceState.getSerializable("item_list");
+                showitemlist();
             }
         }
     }
